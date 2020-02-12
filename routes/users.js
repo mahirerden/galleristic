4d8bc9acde7b12b941b44f891551cd9e55d7e8b8
Jawad Syed <sjahmed@rogers.com>
2020-02-12 18:50:07 -0500

    3rd commit on 12th Feb 2020

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send(`Building new app called Galleristic`);
+// });
+
+/* GET artist page. */
 router.get('/', function(req, res, next) {
-  res.send(`Building new app called Galleristic`);
+  res.render("users", {
+    title1: "Users Section"
+  
+  });
 });
+
 
 module.exports = router;
