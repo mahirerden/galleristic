86fac0724a1c65e903093d15e50ee6a70b69ad32
Jawad Syed <sjahmed@rogers.com>
2020-02-12 16:03:39 -0500

    2nd commit on 12th Feb 2020

---
+++ models/artist.js
@@ -0,0 +1,35 @@
+module.exports = function(sequelize, DataTypes) {
+  var Artist = sequelize.define("Artist", {
+    fullname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+
+  Artist.associate = function(models) {
+    // Associating Artist with Arts
+    // When an Artist is deleted, also delete any associated Arts
+    Artist.hasMany(models.Arts, {
+      onDelete: "cascade"
+    });
+  };
+
+  return Artist;
+};
---
+++ models/arts.js
@@ -0,0 +1,45 @@
+module.exports = function(sequelize, DataTypes) {
+  var Arts = sequelize.define("Arts", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1]
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    thumbnail: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    image: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    }
+  });
+
+  Arts.associate = function(models) {
+    // We're saying that a Arts should belong to an Artist
+    // An Art can't be created without an Artist due to the foreign key constraint
+    Arts.belongsTo(models.Artist, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Arts;
+};
