9d6525515b46bd227cc7796b09c4caee2eea38c2
Jawad Syed <sjahmed@rogers.com>
2020-02-13 23:36:14 -0500

    adding logo in there 13 Fe  2020

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render("index", {
-    title1: "Home Page"
-  
+    title1: "Home Page",
+    logo01: `<img class="thumbnail shadow" src='/images/logo02.png' onclick='this.src="/images/logo02.png"'/>`
   });
 });
 
